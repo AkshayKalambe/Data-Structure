b9eaebd9dbae5ae6db6a71fcf08b4c366ad92af6
akshaykalambe <akshaykalambe10@gmail.com>
2020-10-06 11:07:00 +0530

    push method for Simply_Link_List

---
+++ linkList/singlyLinkList/Singly_Link_List(push).js
@@ -0,0 +1,22 @@
+
+//piece of data- val
+// referance to next node - next
+
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
+    }
+    push(val){
+        
+    }
+}
